549db43319497d7047de39b7b4e9cd558d25cc3a
Stepan Kopriva <stepan.kopriva@oracle.com>
2012-03-15 14:53:47 +0100

    fixing licence header
    Change-Id: If8019c80c9ebba8aa8247447e900007d290750ed

--- core-client/src/main/java/org/glassfish/jersey/client/CustomProvidersFeature.java
+++ core-client/src/main/java/org/glassfish/jersey/client/CustomProvidersFeature.java
@@ -1,5 +1,5 @@
 /*
- DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
  * Copyright (c) 2012 Oracle and/or its affiliates. All rights reserved.
  *
