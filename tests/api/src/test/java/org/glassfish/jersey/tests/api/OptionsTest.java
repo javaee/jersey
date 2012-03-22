5f77a7ee9581d8bf1df7d539e62cbe6b1e596431
Pavel Bucek <pavel.bucek@oracle.com>
2012-03-22 12:35:36 +0100

    adding Options test
    Change-Id: Id004b44c92e8255cc65e05cda9006d2bbe684ebe

---
+++ tests/api/src/test/java/org/glassfish/jersey/tests/api/OptionsTest.java
@@ -0,0 +1,118 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 2012 Oracle and/or its affiliates. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License.  You can
+ * obtain a copy of the License at
+ * http://glassfish.java.net/public/CDDL+GPL_1_1.html
+ * or packager/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at packager/legal/LICENSE.txt.
+ *
+ * GPL Classpath Exception:
+ * Oracle designates this particular file as subject to the "Classpath"
+ * exception as provided by Oracle in the GPL Version 2 section of the License
+ * file that accompanied this code.
+ *
+ * Modifications:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ *
+ * Contributor(s):
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+package org.glassfish.jersey.tests.api;
+
+import org.glassfish.jersey.server.Application;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.junit.Test;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author Pavel Bucek (pavel.bucek at oracle.com)
+ */
+public class OptionsTest extends JerseyTest {
+
+    @Override
+    protected Application configure() {
+        return Application.builder(ResourceConfig.builder().addClasses(HttpOptionsTest.class).build()).build();
+    }
+
+    @Path("/OptionsTest")
+    public static class HttpOptionsTest {
+
+        static String html_content =
+                "<html>" + "<head><title>get text/html</title></head>" +
+                        "<body>get text/html</body></html>";
+
+        @GET
+        public Response getPlain() {
+            return Response.ok("CTS-get text/plain").header("TEST-HEAD", "text-plain").
+                    build();
+        }
+
+        @GET
+        @Produces("text/html")
+        public Response getHtml() {
+            return Response.ok(html_content).header("TEST-HEAD", "text-html").
+                    build();
+        }
+
+        @GET
+        @Path("/sub")
+        public Response getSub() {
+            return Response.ok("TEST-get text/plain").header("TEST-HEAD",
+                    "sub-text-plain").
+                    build();
+        }
+
+        @GET
+        @Path("/sub")
+        @Produces(value = "text/html")
+        public Response headSub() {
+            return Response.ok(html_content).header("TEST-HEAD", "sub-text-html").
+                    build();
+        }
+    }
+
+    /*
+     * Client invokes OPTIONS on a sub resource at /OptionsTest/sub;
+     * which no request method designated for OPTIONS.
+     * Verify that an automatic response is generated.
+     */
+    @Test
+    public void OptionSubTest() {
+        final Response response = target().path("/OptionsTest/sub").request(MediaType.TEXT_HTML_TYPE).options();
+
+        assertTrue(response.getHeaders().getAllowedMethods().contains("GET"));
+        assertTrue(response.getHeaders().getAllowedMethods().contains("HEAD"));
+        assertTrue(response.getHeaders().getAllowedMethods().contains("OPTIONS"));
+
+        assertEquals(response.getHeaders().getMediaType(), MediaType.TEXT_HTML_TYPE);
+    }
+}
