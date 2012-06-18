409f0c6a5ca0bfd80b3d7bb1750a870291a3a6f2
Martin Matula <martin.matula@oracle.com>
2012-06-18 21:39:38 +0200

    Switching to new version of AHC.
    It fixes issue http://java.net/jira/browse/GRIZZLY-1282
    
    Change-Id: Ic92967f4f9006a7d0f7cac0307861e4a790beb12

--- connectors/grizzly-connector/src/test/java/org/glassfish/jersey/grizzly/connector/FollowRedirectsTest.java
+++ connectors/grizzly-connector/src/test/java/org/glassfish/jersey/grizzly/connector/FollowRedirectsTest.java
@@ -71,7 +71,7 @@ public class FollowRedirectsTest extends JerseyTest {
         @GET
         @Path("redirect")
         public Response redirect() {
-            return Response.status(302).location(UriBuilder.fromResource(RedirectResource.class).build()).build();
+            return Response.seeOther(UriBuilder.fromResource(RedirectResource.class).build()).build();
         }
     }
 
@@ -97,6 +97,6 @@ public class FollowRedirectsTest extends JerseyTest {
     public void testDontFollow() {
         WebTarget t = target("test/redirect");
         t.configuration().setProperty(ClientProperties.FOLLOW_REDIRECTS, false);
-        assertEquals(302, t.request().get().getStatus());
+        assertEquals(303, t.request().get().getStatus());
     }
 }
