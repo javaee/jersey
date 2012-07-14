8e7e7174f143cd5c4f2f117952c46d76cb277733
Jakub Podlesak <jakub.podlesak@oracle.com>
2012-07-14 02:05:02 +0200

    Fix for 1288, added missing OSGi headers to the SSE module  - an OSGi test added to the tests/osgi/functional module (fixed redundant deps in the pom there)  - added media sse module to the Jersey GF bundle
    Change-Id: I77380899d43cbef9c4ca758b2d8f2824538f0c18

---
+++ tests/osgi/functional/src/test/java/org/glassfish/jersey/osgi/test/basic/SseTest.java
@@ -0,0 +1,200 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 2012 Oracle and/or its affiliates. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License.  You can
+ * obtain a copy of the License at
+ * http://glassfish.java.net/public/CDDL+GPL_1_1.html
+ * or packager/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at packager/legal/LICENSE.txt.
+ *
+ * GPL Classpath Exception:
+ * Oracle designates this particular file as subject to the "Classpath"
+ * exception as provided by Oracle in the GPL Version 2 section of the License
+ * file that accompanied this code.
+ *
+ * Modifications:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ *
+ * Contributor(s):
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+package org.glassfish.jersey.osgi.test.basic;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientFactory;
+import javax.ws.rs.core.UriBuilder;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.media.sse.EventChannel;
+import org.glassfish.jersey.media.sse.EventSource;
+import org.glassfish.jersey.media.sse.InboundEvent;
+import org.glassfish.jersey.media.sse.OutboundEvent;
+import org.glassfish.jersey.media.sse.OutboundEventWriter;
+import org.glassfish.jersey.osgi.test.util.Helper;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import org.ops4j.pax.exam.Inject;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.Configuration;
+import org.ops4j.pax.exam.junit.JUnit4TestRunner;
+
+import org.osgi.framework.BundleContext;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+
+import static org.ops4j.pax.exam.CoreOptions.*;
+import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.repositories;
+
+
+/**
+ * Basic test for SSE module OSGification.
+ *
+ * @author Jakub Podlesak (jakub.podlesak at oracle.com)
+ */
+@RunWith(JUnit4TestRunner.class)
+public class SseTest {
+
+    private static final Logger LOGGER = Logger.getLogger(SseTest.class.getName());
+    private static final int port = Helper.getEnvVariable("jersey.test.port", 8080);
+    private static final String CONTEXT = "/jersey";
+    private static final URI baseUri = UriBuilder.fromUri("http://localhost").port(Helper.getEnvVariable("jersey.test.port", 8080)).path(CONTEXT).build();
+    @Inject
+    protected BundleContext bundleContext;
+
+    @Configuration
+    public static Option[] configuration() {
+
+        Option[] options = options(
+                //                systemProperty("org.ops4j.pax.logging.DefaultServiceLog.level").value("FINEST"),
+                systemProperty("jersey.test.port").value(String.valueOf(port)),
+                systemPackage("sun.misc"),
+                // define maven repository
+                repositories(
+                "http://repo1.maven.org/maven2",
+                "http://repository.apache.org/content/groups/snapshots-group",
+                "http://repository.ops4j.org/maven2",
+                "http://svn.apache.org/repos/asf/servicemix/m2-repo",
+                "http://repository.springsource.com/maven/bundles/release",
+                "http://repository.springsource.com/maven/bundles/external",
+                "http://maven.java.net/content/repositories/snapshots"),
+                // log
+                //                mavenBundle("org.ops4j.pax.logging", "pax-logging-api", "1.4"),
+                //                mavenBundle("org.ops4j.pax.logging", "pax-logging-service", "1.4"),
+
+                // felix config admin
+                //                mavenBundle("org.apache.felix", "org.apache.felix.configadmin", "1.2.4"),
+
+                // felix preference service
+                //                mavenBundle("org.apache.felix", "org.apache.felix.prefs","1.0.2"),
+
+                // HTTP SPEC
+                //mavenBundle("org.apache.geronimo.specs","geronimo-servlet_2.5_spec","1.1.2"),
+                // Google Guava
+                mavenBundle().groupId("com.googlecode.guava-osgi").artifactId("guava-osgi").versionAsInProject(),
+                // HK2
+                mavenBundle().groupId("org.glassfish.hk2").artifactId("hk2-api").versionAsInProject(),
+                mavenBundle().groupId("org.glassfish.hk2").artifactId("osgi-resource-locator").versionAsInProject(),
+                mavenBundle().groupId("org.glassfish.hk2").artifactId("auto-depends").versionAsInProject(),
+                mavenBundle().groupId("org.glassfish.hk2.external").artifactId("javax.inject").versionAsInProject(),
+                mavenBundle().groupId("org.glassfish.hk2.external").artifactId("asm-all-repackaged").versionAsInProject(),
+                // JAX-RS API
+                mavenBundle().groupId("javax.ws.rs").artifactId("javax.ws.rs-api").versionAsInProject(),
+                // javax.annotation
+                wrappedBundle(mavenBundle().groupId("javax.annotation").artifactId("jsr250-api").versionAsInProject()),
+                // Jersey bundles
+                mavenBundle().groupId("org.glassfish.jersey.core").artifactId("jersey-common").versionAsInProject(),
+                mavenBundle().groupId("org.glassfish.jersey.core").artifactId("jersey-server").versionAsInProject(),
+                mavenBundle().groupId("org.glassfish.jersey.core").artifactId("jersey-client").versionAsInProject(),
+                mavenBundle().groupId("org.glassfish.jersey.media").artifactId("jersey-media-sse").versionAsInProject(),
+                mavenBundle().groupId("org.glassfish.jersey.containers").artifactId("jersey-container-grizzly2-http").versionAsInProject(),
+                // Grizzly
+                mavenBundle().groupId("org.glassfish.grizzly").artifactId("grizzly-http-server").versionAsInProject(),
+                mavenBundle().groupId("org.glassfish.grizzly").artifactId("grizzly-rcm").versionAsInProject(),
+                mavenBundle().groupId("org.glassfish.grizzly").artifactId("grizzly-http").versionAsInProject(),
+                mavenBundle().groupId("org.glassfish.grizzly").artifactId("grizzly-framework").versionAsInProject(),
+                mavenBundle().groupId("org.glassfish.gmbal").artifactId("gmbal-api-only").versionAsInProject(),
+                mavenBundle().groupId("org.glassfish.external").artifactId("management-api").versionAsInProject(),
+                // start felix framework
+                felix());
+
+        return options;
+    }
+
+    @Path("/sse")
+    public static class SseResource {
+
+        @GET
+        @Produces(EventChannel.SERVER_SENT_EVENTS)
+        public EventChannel getIt() throws IOException {
+            final EventChannel result = new EventChannel();
+            result.write(new OutboundEvent.Builder().name("event1").data(String.class, "ping").build());
+            result.write(new OutboundEvent.Builder().name("event2").data(String.class, "pong").build());
+            result.close();
+            return result;
+        }
+    }
+
+    @Test
+    public void testSse() throws Exception {
+        final ResourceConfig resourceConfig = new ResourceConfig(SseResource.class, OutboundEventWriter.class);
+        final HttpServer server = GrizzlyHttpServerFactory.createHttpServer(baseUri, resourceConfig);
+
+        Client c = ClientFactory.newClient();
+        c.configuration().register(OutboundEventWriter.class);
+
+        final ExecutorService threadPool = Executors.newSingleThreadExecutor();
+        final List<String> data = new LinkedList<String>();
+
+        new EventSource(c.target(baseUri).path("/sse"), threadPool) {
+
+            @Override
+            public void onEvent(InboundEvent event) {
+                try {
+                    data.add(event.getData());
+                } catch (IOException e) {
+                    // ignore
+                }
+            }
+        };
+        threadPool.awaitTermination(2, TimeUnit.SECONDS);
+
+        assertEquals(2, data.size());
+
+        server.stop();
+    }
+}
