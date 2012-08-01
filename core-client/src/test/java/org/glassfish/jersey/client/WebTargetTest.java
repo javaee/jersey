9c21dda50686edd98fc38a448f1915b8f7d53b76
Marek Potociar <marek.potociar@oracle.com>
2012-08-01 21:35:33 +0200

    Fixed JERSEY-1321.
    - apdated WebTarget implementation
    - added covering unit tests
    
    Change-Id: I2d4ef50fab94204ff9a97151f9cf978a4157ea0e
    Signed-off-by: Marek Potociar <marek.potociar@oracle.com>

--- core-client/src/test/java/org/glassfish/jersey/client/WebTargetTest.java
+++ core-client/src/test/java/org/glassfish/jersey/client/WebTargetTest.java
@@ -39,10 +39,20 @@
  */
 package org.glassfish.jersey.client;
 
+import java.net.URI;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.ws.rs.client.ClientFactory;
+import javax.ws.rs.core.MultivaluedHashMap;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.UriBuilder;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 /**
@@ -72,6 +82,149 @@ public class WebTargetTest {
             fail("IllegalStateException was expected.");
         } catch (IllegalStateException e) {
             // ignore
+        }
+    }
+
+    @Test
+    public void testPathParams() {
+        URI uri;
+        UriBuilder uriBuilder;
+
+        uri = target.pathParam("a", "v").pathParam("a", null).getUri();
+        assertEquals("/", uri.toString());
+
+        uri = target.path("{a}").pathParam("a", "v").getUri();
+        assertEquals("/v", uri.toString());
+
+        uriBuilder = target.path("{a}").pathParam("a", "v").pathParam("a", null).getUriBuilder();
+        assertEquals("/%7Ba%7D", uriBuilder.build().toString());
+
+        final Map<String, Object> params = new HashMap<String, Object>(2);
+        params.put("a", "w1");
+        params.put("b", null);
+        try {
+            target.path("{a}").path("{b}").pathParam("a", "v1").pathParam("b", "v2")
+                    .pathParams(params).getUri();
+            fail("IllegalStateException expected - template variable 'b' should have no value.");
+        } catch (IllegalStateException ex) {
+            // expected
+        }
+
+        try {
+            target.pathParams(null);
+            fail("NullPointerException expected.");
+        } catch (NullPointerException ex) {
+            // expected
+        }
+    }
+
+    @Test
+    @Ignore("Un-ignore once JERSEY-1329 is fixed.")
+    // TODO un-ignore
+    public void testGetUriBuilder() {
+        final Map<String, Object> params = new HashMap<String, Object>(2);
+        params.put("a", "w1");
+        params.put("b", null);
+        UriBuilder uriBuilder = target.path("{a}").path("{b}").pathParam("a", "v1").pathParam("b", "v2")
+                .pathParams(params).getUriBuilder();
+        assertEquals("/w1/%7Bb%7D", uriBuilder.build().toString());
+    }
+
+    @Test
+    public void testQueryParams() {
+        URI uri;
+
+        uri = target.path("a").queryParam("q", "v1", "v2").queryParam("q").getUri();
+        assertEquals("/a", uri.toString());
+
+        uri = target.path("a").queryParam("q", "v1", "v2").queryParam("q", (Object) null).getUri();
+        assertEquals("/a", uri.toString());
+
+        uri = target.path("a").queryParam("q", "v1", "v2").queryParam("q", (Object[]) null).getUri();
+        assertEquals("/a", uri.toString());
+
+        uri = target.path("a").queryParam("q", "v1", "v2").queryParam("q", new Object[]{}).getUri();
+        assertEquals("/a", uri.toString());
+
+        uri = target.path("a").queryParam("q", "v").getUri();
+        assertEquals("/a?q=v", uri.toString());
+
+        uri = target.path("a").queryParam("q1", "v1").queryParam("q2", "v2").queryParam("q1", (Object) null).getUri();
+        assertEquals("/a?q2=v2", uri.toString());
+
+        try {
+            target.queryParam("q", "v1", null, "v2", null);
+            fail("NullPointerException expected.");
+        } catch (NullPointerException ex) {
+            // expected
+        }
+
+        {
+            final MultivaluedMap<String, Object> params = new MultivaluedHashMap<String, Object>(2);
+            params.add("q1", "w1");
+            params.add("q1", "w2");
+            params.add("q2", null);
+            uri = target.path("a").queryParam("q1", "v1").queryParam("q2", "v2").queryParams(params).getUri();
+            assertEquals("/a?q1=v1&q1=w1&q1=w2", uri.toString());
+        }
+
+        try {
+            target.queryParams(null);
+            fail("NullPointerException expected.");
+        } catch (NullPointerException ex) {
+            // expected
+        }
+
+        try {
+            // null-values supporting multivalued hash map
+            final MultivaluedMap<String, Object> params = new MultivaluedHashMap<String, Object>(2) {
+                @Override
+                protected void addNull(List<Object> values) {
+                    values.add(null);
+                }
+
+                @Override
+                protected void addFirstNull(List<Object> values) {
+                    values.add(null);
+                }
+            };
+            params.add("q1", "w1");
+            params.add("q1", null);
+            params.add("q2", null);
+            target.path("a").queryParam("q1", "v1").queryParam("q2", "v2").queryParams(params);
+            fail("NullPointerException expected.");
+        } catch (NullPointerException ex) {
+            // expected
+        }
+    }
+
+    @Test
+    public void testMatrixParams() {
+        URI uri;
+
+        uri = target.path("a").matrixParam("q", "v1", "v2").matrixParam("q").getUri();
+        assertEquals("/a", uri.toString());
+
+        uri = target.path("a").matrixParam("q", "v1", "v2").matrixParam("q", (Object) null).getUri();
+        assertEquals("/a", uri.toString());
+
+        uri = target.path("a").matrixParam("q", "v1", "v2").matrixParam("q", (Object[]) null).getUri();
+        assertEquals("/a", uri.toString());
+
+        uri = target.path("a").matrixParam("q", "v1", "v2").matrixParam("q", new Object[]{}).getUri();
+        assertEquals("/a", uri.toString());
+
+        uri = target.path("a").matrixParam("q", "v").getUri();
+        assertEquals("/a;q=v", uri.toString());
+
+        uri = target.path("a").matrixParam("q1", "v1").matrixParam("q2", "v2").matrixParam("q1", (Object) null).getUri();
+        assertEquals("/a;q2=v2", uri.toString());
+
+        try {
+            target.matrixParam("q", "v1", null, "v2", null);
+            fail("NullPointerException expected.");
+        } catch (NullPointerException ex) {
+            // expected
         }
     }
 }
