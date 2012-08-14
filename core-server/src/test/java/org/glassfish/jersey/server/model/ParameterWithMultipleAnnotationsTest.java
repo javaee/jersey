4563afbecd4225eb87093bea8a72928fb52b5c3a
James Strachan <james.strachan@gmail.com>
2012-08-14 13:06:48 +0100

    moved DummyAnnotation into the test case and avoid the wrong license header

--- core-server/src/test/java/org/glassfish/jersey/server/model/ParameterWithMultipleAnnotationsTest.java
+++ core-server/src/test/java/org/glassfish/jersey/server/model/ParameterWithMultipleAnnotationsTest.java
@@ -69,6 +69,11 @@ public class ParameterWithMultipleAnnotationsTest {
 
         }
     }
+
+    @java.lang.annotation.Target({java.lang.annotation.ElementType.PARAMETER, java.lang.annotation.ElementType.METHOD, java.lang.annotation.ElementType.FIELD})
+    @java.lang.annotation.Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
+    public @interface DummyAnnotation {
+    }
 }
 
 
