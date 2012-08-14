212118dfbd47f3211dedf21638903c58a6b2cc15
James Strachan <james.strachan@gmail.com>
2012-08-14 10:21:37 +0100

    reverted the IDE reorder of imports

--- core-server/src/main/java/org/glassfish/jersey/server/model/Parameter.java
+++ core-server/src/main/java/org/glassfish/jersey/server/model/Parameter.java
@@ -39,19 +39,32 @@
  */
 package org.glassfish.jersey.server.model;
 
-import org.glassfish.jersey.internal.util.ReflectionHelper;
-import org.glassfish.jersey.internal.util.collection.ClassTypePair;
-
-import javax.ws.rs.*;
-import javax.ws.rs.core.Context;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.AnnotatedElement;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 import java.lang.reflect.Type;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.WeakHashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import javax.ws.rs.CookieParam;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.Encoded;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.MatrixParam;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.Uri;
+import javax.ws.rs.core.Context;
+
+import org.glassfish.jersey.internal.util.ReflectionHelper;
+import org.glassfish.jersey.internal.util.collection.ClassTypePair;
 
 /**
  * Method parameter model.
