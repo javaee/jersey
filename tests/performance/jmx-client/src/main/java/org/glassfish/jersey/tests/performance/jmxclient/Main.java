a2abd289a712dd144ffd0b0eb57f6e646ea50307
Jakub Podlesak <jakub.podlesak@oracle.com>
2012-10-18 18:13:41 +0200

    performance test cases added
    Change-Id: Iab45a108248581829d6d2f8c917edaa46a2be888

---
+++ tests/performance/jmx-client/src/main/java/org/glassfish/jersey/tests/performance/jmxclient/Main.java
@@ -0,0 +1,109 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 2012 Oracle and/or its affiliates. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License.  You can
+ * obtain a copy of the License at
+ * https://glassfish.dev.java.net/public/CDDL+GPL_1_1.html
+ * or packager/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at packager/legal/LICENSE.txt.
+ *
+ * GPL Classpath Exception:
+ * Oracle designates this particular file as subject to the "Classpath"
+ * exception as provided by Oracle in the GPL Version 2 section of the License
+ * file that accompanied this code.
+ *
+ * Modifications:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ *
+ * Contributor(s):
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+package org.glassfish.jersey.tests.performance.jmxclient;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+import javax.management.MBeanServerConnection;
+import javax.management.ObjectName;
+import javax.management.remote.JMXConnector;
+import javax.management.remote.JMXConnectorFactory;
+import javax.management.remote.JMXServiceURL;
+
+/**
+ * JMX Client entry point.
+ *
+ * @author Jakub Podlesak (jakub.podlesak at oracle.com)
+ */
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+//      e.g. "service:jmx:rmi:///jndi/rmi://sysifos.cz.oracle.com:11112/jmxrmi"
+        final String jmxUrl = args[0];
+//      e.g. "org.glassfish.jersey.test.performance.interceptor.dynamic:type=DynamicallyBoundInterceptorResource,name=gets"
+        final String mBeanName = args[1];
+//      e.g. "OneMinuteRate"
+        final String mBeanAttrName = args[2];
+//      e.g. 50
+        final int sampleCount = Integer.parseInt(args[3]);
+//      e.g. "phishing.properties"
+        final String propertiesFile = args[4];
+
+        System.out.printf("JMX URL = %s\nMBean = %s\nattribute = %s\nsamples = %d\nfilename = %s\n"
+                + "Going to connect...\n",
+                jmxUrl, mBeanName, mBeanAttrName, sampleCount, propertiesFile);
+
+        final JMXServiceURL url = new JMXServiceURL(jmxUrl);
+        final JMXConnector jmxc = JMXConnectorFactory.connect(url, null);
+        final MBeanServerConnection mBeanServer = jmxc.getMBeanServerConnection();
+        final ObjectName mBeanObjectName = new ObjectName(mBeanName);
+
+        System.out.println("Connected...");
+
+        double totalSum = 0;
+        int samplesTaken = 0;
+
+        for (int i = 0; i < sampleCount; i++) {
+            Thread.sleep(5000);
+
+            Double sample = (Double) mBeanServer.getAttribute(mBeanObjectName, mBeanAttrName);
+
+            System.out.printf("OMR[%d]=%f\n", i, sample);
+
+            totalSum += sample;
+            samplesTaken++;
+        }
+
+        jmxc.close();
+
+        final double result = totalSum / samplesTaken;
+        writeResult(result, propertiesFile);
+
+        System.out.printf("\nAverage=%f\n", result);
+    }
+
+    private static void writeResult(double resultValue, String propertiesFile) throws IOException {
+        Properties resultProps = new Properties();
+        resultProps.put("YVALUE", Double.toString(resultValue));
+        resultProps.store(new FileOutputStream(propertiesFile), null);
+    }
+}
---
+++ tests/performance/test-cases/filter-dynamic/src/main/java/org/glassfish/jersey/tests/performance/filter/dynamic/DynamicallyBoundFilterResource.java
@@ -0,0 +1,98 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 2012 Oracle and/or its affiliates. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License.  You can
+ * obtain a copy of the License at
+ * https://glassfish.dev.java.net/public/CDDL+GPL_1_1.html
+ * or packager/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at packager/legal/LICENSE.txt.
+ *
+ * GPL Classpath Exception:
+ * Oracle designates this particular file as subject to the "Classpath"
+ * exception as provided by Oracle in the GPL Version 2 section of the License
+ * file that accompanied this code.
+ *
+ * Modifications:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ *
+ * Contributor(s):
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+package org.glassfish.jersey.tests.performance.filter.dynamic;
+
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.yammer.metrics.Metrics;
+import com.yammer.metrics.core.TimerContext;
+
+import java.util.concurrent.TimeUnit;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+
+/**
+ * Filtered resource.
+ *
+ * @author Jakub Podlesak (jakub.podlesak at oracle.com)
+ */
+@Path(JerseyApp.ROOT_PATH)
+@Consumes(MediaType.TEXT_PLAIN)
+@Produces(MediaType.TEXT_PLAIN)
+public class DynamicallyBoundFilterResource {
+
+    private static final com.yammer.metrics.core.Timer postTimer =
+            Metrics.newTimer(DynamicallyBoundFilterResource.class, "posts", TimeUnit.MILLISECONDS, TimeUnit.SECONDS);
+    private static final com.yammer.metrics.core.Timer getTimer =
+            Metrics.newTimer(DynamicallyBoundFilterResource.class, "gets", TimeUnit.MILLISECONDS, TimeUnit.SECONDS);
+    private static final com.yammer.metrics.core.Timer putTimer =
+            Metrics.newTimer(DynamicallyBoundFilterResource.class, "puts", TimeUnit.MILLISECONDS, TimeUnit.SECONDS);
+
+    @POST
+    public String echo(final String text) {
+        final TimerContext timer = postTimer.time();
+        try {
+            return text;
+        } finally {
+            timer.stop();
+        }
+    }
+
+    @PUT
+    public void put(final String text) {
+        final TimerContext timer = putTimer.time();
+        timer.stop();
+    }
+
+    @GET
+    public String get() {
+        final TimerContext timer = getTimer.time();
+        try {
+            return "text";
+        } finally {
+            timer.stop();
+        }
+    }
+}
