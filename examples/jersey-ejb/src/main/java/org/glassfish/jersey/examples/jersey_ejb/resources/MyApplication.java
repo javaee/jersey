d4fa9f6202794942b15c3de647b6df96e8316681
Jakub Podlesak <jakub.podlesak@oracle.com>
2013-01-24 19:11:18 +0100

    fixed validation issue in jersey-ejb example
    Change-Id: I6e2590fc7591fbee7521181b9df09ab223df177e

--- examples/jersey-ejb/src/main/java/org/glassfish/jersey/examples/jersey_ejb/resources/MyApplication.java
+++ examples/jersey-ejb/src/main/java/org/glassfish/jersey/examples/jersey_ejb/resources/MyApplication.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2010-2012 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010-2013 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -61,7 +61,6 @@ public class MyApplication extends Application {
         final Set<Class<?>> classes = new HashSet<Class<?>>();
         // register root resources/providers
         classes.add(MessageBoardRootResource.class);
-        classes.add(MessageBoardResourceBean.class);
         classes.add(MessageHolderSingletonBean.class);
         classes.add(NotFoundExceptionMapper.class);
         classes.add(MessageWriter.class);
