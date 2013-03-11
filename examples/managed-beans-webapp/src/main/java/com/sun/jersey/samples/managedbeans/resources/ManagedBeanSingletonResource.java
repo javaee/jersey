e1bdbeff45c6c36ce25516c07fbb4b50e2c93ac1
Jakub Podlesak <jakub.podlesak@oracle.com>
2013-03-11 19:24:25 +0100

    Added managed bean web app example to test JERSEY-1576
    Change-Id: I7064f72d6a6c313e97cb8c7ff4174aa8e0fbec71

---
+++ examples/managed-beans-webapp/src/main/java/com/sun/jersey/samples/managedbeans/resources/ManagedBeanSingletonResource.java
@@ -0,0 +1,159 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 2010-2013 Oracle and/or its affiliates. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License.  You can
+ * obtain a copy of the License at
+ * http://glassfish.java.net/public/CDDL+GPL_1_1.html
+ * or packager/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at packager/legal/LICENSE.txt.
+ *
+ * GPL Classpath Exception:
+ * Oracle designates this particular file as subject to the "Classpath"
+ * exception as provided by Oracle in the GPL Version 2 section of the License
+ * file that accompanied this code.
+ *
+ * Modifications:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ *
+ * Contributor(s):
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+
+package com.sun.jersey.samples.managedbeans.resources;
+
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+import javax.annotation.ManagedBean;
+import javax.annotation.Resource;
+
+import javax.inject.Singleton;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
+
+/**
+ * JAX-RS root resource treated as Java EE managed bean in singleton scope.
+ *
+ * @author Paul Sandoz (paul.sandoz at oracle.com)
+ * @author Jakub Podlesak (jakub.podlesak at oracle.com)
+ */
+@Path("/managedbean/singleton")
+@Singleton
+@ManagedBean
+public class ManagedBeanSingletonResource {
+
+    /**
+     * Initial value should get injected by Java EE container.
+     */
+    @Resource(name="injectedResource") int counter = 0;
+
+    @Context UriInfo ui;
+
+    private EntityManager entityManager;
+
+    /**
+     * Set entity manager based on injected entity manager factory.
+     *
+     * @param em entity manager factory injected by Java EE container.
+     */
+    @PersistenceUnit(unitName = "ManagedBeansPU")
+    void setEntityManager(EntityManagerFactory em) {
+        entityManager = em.createEntityManager();
+    }
+
+    /**
+     * Provide textual representation of the internal counter.
+     * @return
+     */
+    @GET
+    @Produces("text/plain")
+    public String getMessage() {
+       return Integer.toString(counter++);
+    }
+
+    /**
+     * Reset internal counter.
+     * @param i new counter value to be set.
+     */
+    @PUT
+    @Produces("text/plain")
+    public void putMessage(int i) {
+       counter = i;
+    }
+
+    /**
+     * Throw a runtime exception, so that it could be mapped.
+     *
+     * @return nothing, just throw the custom exception.
+     */
+    @Path("exception")
+    public String getException() {
+        throw new ManagedBeanException();
+    }
+
+    /**
+     * Get JPA widget value.
+     *
+     * @param id widget id.
+     * @return value of the widget or 404 if given widget id is not found.
+     */
+    @Path("widget/{id: \\d+}")
+    @GET
+    public String getWidget(@PathParam("id") int id) {
+        try {
+            return entityManager.find(Widget.class, id).val;
+        } catch (NullPointerException ignored) {
+            throw new WebApplicationException(Response.Status.NOT_FOUND);
+        }
+    }
+
+    /**
+     * Set new widget value.
+     * @param id widget id.
+     * @param val new value to be associated with above specified id.
+     */
+    @Path("widget/{id: \\d+}")
+    @PUT
+    public void putWidget(@PathParam("id") int id, String val) {
+        entityManager.merge(new Widget(id, val));
+    }
+
+    /**
+     * Remove widget with given id.
+     * @param id id of the widget to be removed.
+     */
+    @Path("widget/{id: \\d+}")
+    @DELETE
+    public void deleteWidget(@PathParam("id") int id) {
+        entityManager.remove(entityManager.find(Widget.class, id));
+    }
+}
---
+++ examples/managed-beans-webapp/src/test/java/com/sun/jersey/samples/managedbeans/ManagedBeanWebAppTest.java
@@ -0,0 +1,183 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 2010-2013 Oracle and/or its affiliates. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License.  You can
+ * obtain a copy of the License at
+ * http://glassfish.java.net/public/CDDL+GPL_1_1.html
+ * or packager/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at packager/legal/LICENSE.txt.
+ *
+ * GPL Classpath Exception:
+ * Oracle designates this particular file as subject to the "Classpath"
+ * exception as provided by Oracle in the GPL Version 2 section of the License
+ * file that accompanied this code.
+ *
+ * Modifications:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ *
+ * Contributor(s):
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+
+package com.sun.jersey.samples.managedbeans;
+
+import java.net.URI;
+
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+
+import org.glassfish.jersey.message.internal.MediaTypes;
+import org.glassfish.jersey.test.JerseyTest;
+
+import com.sun.jersey.samples.managedbeans.resources.MyApplication;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Invocation;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+
+/**
+ * Main test for the Managed Beans web application.
+ * The application must be deployed and running on a standalone GlassFish container.
+ * To run the tests then, you just launch the following command:
+ * <pre>
+ * mvn -Ptest \
+ *       -Djersey.config.test.container.factory=org.glassfish.jersey.test.external.ExternalTestContainerFactory \
+ *       -Djersey.config.test.container.port=8080 test</pre>
+ *
+ * @author Naresh Srinivas Bhimisetty
+ * @author Jakub Podlesak (jakub.podlesak at oracle.com)
+ */
+public class ManagedBeanWebAppTest extends JerseyTest {
+
+    @Override
+    protected Application configure() {
+        return new MyApplication();
+    }
+
+    @Override
+    protected URI getBaseUri() {
+        return UriBuilder.fromUri(super.getBaseUri()).path("managed-beans-webapp").path("app").build();
+    }
+
+    /**
+     * Test that provided query parameter makes it back.
+     */
+    @Test
+    public void testPerRequestResource() {
+        WebTarget perRequest = target().path("managedbean/per-request");
+
+        String responseMsg = perRequest.queryParam("x", "X").request().get(String.class);
+        assertThat(responseMsg, containsString("X"));
+
+        responseMsg = perRequest.queryParam("x", "hi there").request().get(String.class);
+        assertThat(responseMsg, containsString("hi there"));
+    }
+
+    /**
+     * Test that singleton counter gets incremented with each call and can be reset.
+     */
+    @Test
+    public void testSingletonResource() {
+        WebTarget singleton = target().path("managedbean/singleton");
+
+        String responseMsg = singleton.request().get(String.class);
+        assertThat(responseMsg, containsString("3"));
+
+        responseMsg = singleton.request().get(String.class);
+        assertThat(responseMsg, containsString("4"));
+
+        singleton.request().put(Entity.text("1"));
+
+        responseMsg = singleton.request().get(String.class);
+        assertThat(responseMsg, containsString("1"));
+
+        responseMsg = singleton.request().get(String.class);
+        assertThat(responseMsg, containsString("2"));
+    }
+
+    /**
+     * Test the JPA backend.
+     */
+    @Test
+    public void testWidget() {
+        WebTarget target = target().path("managedbean/singleton/widget");
+
+        final WebTarget widget = target.path("1");
+
+        assertThat(widget.request().get().getStatus(), is(404));
+
+        widget.request().put(Entity.text("One"));
+        assertThat(widget.request().get(String.class), is("One"));
+
+        widget.request().put(Entity.text("Two"));
+        assertThat(widget.request().get(String.class), is("Two"));
+
+        assertThat(widget.request().delete().getStatus(), is(204));
+
+        assertThat(widget.request().get().getStatus(), is(404));
+    }
+
+    /**
+     * Test exceptions are properly mapped.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testExceptionMapper() {
+
+        WebTarget singletonTarget = target().path("managedbean/singleton/exception");
+        WebTarget perRequestTarget = target().path("managedbean/per-request/exception");
+
+        _testExceptionOutput(singletonTarget, "singleton");
+        _testExceptionOutput(perRequestTarget, "per-request");
+    }
+
+    /**
+     * Test a non empty WADL is generated.
+     */
+    @Test
+    public void testApplicationWadl() {
+        WebTarget wadl = target().path("application.wadl");
+        String wadlDoc = wadl.request(MediaTypes.WADL).get(String.class);
+
+        assertThat(wadlDoc.length(), is(not(0)));
+    }
+
+
+    private void _testExceptionOutput(WebTarget exceptionTarget, String thatShouldBePresentInResponseBody) {
+
+        Response exceptionResponse = exceptionTarget.request().get();
+        assertThat(exceptionResponse.getStatus(), is(500));
+
+        final String responseBody = exceptionResponse.readEntity(String.class);
+
+        assertThat(responseBody, containsString("ManagedBeanException"));
+        assertThat(responseBody, containsString(thatShouldBePresentInResponseBody));
+    }
+}
