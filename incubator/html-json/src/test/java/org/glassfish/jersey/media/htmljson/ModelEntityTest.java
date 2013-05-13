acd2b64b82dfe1ad22682f3fcca5ee1424a57c9b
Jaroslav Tulach <jaroslav.tulach@apidesign.org>
2013-05-13 09:43:13 +0200

    Getting ready to be moved out of incubator: Use org.glassfish.jersey.media.htmljson package

--- incubator/html-json/src/test/java/org/glassfish/jersey/media/htmljson/ModelEntityTest.java
+++ incubator/html-json/src/test/java/org/glassfish/jersey/media/htmljson/ModelEntityTest.java
@@ -37,7 +37,7 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
-package org.glassfish.jersey.incubator.html.json;
+package org.glassfish.jersey.media.htmljson;
 
 
 import javax.ws.rs.GET;
