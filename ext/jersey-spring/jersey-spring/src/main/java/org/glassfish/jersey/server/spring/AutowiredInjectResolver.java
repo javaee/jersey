eade69fba166b64b433bf9058924b71d2bbfc5c5
marko asplund <marko.asplund@ixonos.com>
2013-07-30 23:55:29 +0300

    JERSEY-1957: minor AutowiredInjectResolver refactoring

--- ext/jersey-spring/jersey-spring/src/main/java/org/glassfish/jersey/server/spring/AutowiredInjectResolver.java
+++ ext/jersey-spring/jersey-spring/src/main/java/org/glassfish/jersey/server/spring/AutowiredInjectResolver.java
@@ -9,6 +9,9 @@ import org.springframework.context.ApplicationContext;
 
 import javax.inject.Singleton;
 import java.lang.reflect.AnnotatedElement;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.Map;
 import java.util.logging.Logger;
 
 /**
@@ -19,11 +22,11 @@ import java.util.logging.Logger;
 @Singleton
 public class AutowiredInjectResolver implements InjectionResolver<Autowired> {
     private static final Logger LOGGER = Logger.getLogger(AutowiredInjectResolver.class.getName());
-    private InjectResolverHelper injectResolverHelper;
+    private ApplicationContext ctx;
 
     public AutowiredInjectResolver(ApplicationContext ctx) {
         LOGGER.fine("AutowiredInjectResolver()");
-        injectResolverHelper = new InjectResolverHelper(ctx);
+        this.ctx = ctx;
     }
 
     @Override
@@ -37,7 +40,32 @@ public class AutowiredInjectResolver implements InjectionResolver<Autowired> {
                 beanName = an.value();
             }
         }
-        return injectResolverHelper.getBeanFromSpringContext(beanName, injectee.getRequiredType());
+        return getBeanFromSpringContext(beanName, injectee.getRequiredType());
+    }
+
+    private Object getBeanFromSpringContext(String beanName, Type beanType) {
+        Class<?> bt = getClassFromType(beanType);
+        if(beanName != null) {
+            return ctx.getBean(beanName, bt);
+        }
+        Map<String, ?> beans = ctx.getBeansOfType(bt);
+        if(beans == null || beans.size() != 1) {
+            LOGGER.warning("no beans found, resolve failed for type "+beanType);
+            return null;
+        }
+        Object o = beans.values().iterator().next();
+        LOGGER.finer("resolve: "+o);
+        return o;
+    }
+
+    private Class<?> getClassFromType(Type type) {
+        if (type instanceof Class) return (Class<?>) type;
+        if (type instanceof ParameterizedType) {
+            ParameterizedType pt = (ParameterizedType) type;
+
+            return (Class<?>) pt.getRawType();
+        }
+        return null;
     }
 
     @Override
