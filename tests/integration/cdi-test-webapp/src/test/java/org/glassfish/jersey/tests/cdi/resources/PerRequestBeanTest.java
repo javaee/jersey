9fc2a5d18241c506489ffb4a92d4d245bc5bde13
Marek Potociar <marek.potociar@oracle.com>
2013-09-02 19:09:26 +0200

    Jersey project build profiles refactoring.
    - Removed profiles: attach-src, attach-src-and-docs, all-tests, examples-source-zip
      - Producing source jars made part of a default build lifecycle
      - Running all tests made part of the default lifecycle
      - Producing example zips made part of release profile
    
    - Renamed profile: maven-project-info-reports-plugin -> project-info
    
    - Fixed copyright headers
    
    - Updated readme files, comments
    
    Change-Id: I855bf1e4ca885410e6fc1f3a324a62add6c622c5
    Signed-off-by: Marek Potociar <marek.potociar@oracle.com>

--- tests/integration/cdi-test-webapp/src/test/java/org/glassfish/jersey/tests/cdi/resources/PerRequestBeanTest.java
+++ tests/integration/cdi-test-webapp/src/test/java/org/glassfish/jersey/tests/cdi/resources/PerRequestBeanTest.java
@@ -7,7 +7,7 @@
  * General Public License Version 2 only ("GPL") or the Common Development
  * and Distribution License("CDDL") (collectively, the "License").  You
  * may not use this file except in compliance with the License.  You can
- * obtain x copy of the License at
+ * obtain a copy of the License at
  * http://glassfish.java.net/public/CDDL+GPL_1_1.html
  * or packager/legal/LICENSE.txt.  See the License for the specific
  * language governing permissions and limitations under the License.
@@ -29,7 +29,7 @@
  * If you wish your version of this file to be governed by only the CDDL or
  * only the GPL Version 2, indicate your decision by adding "[Contributor]
  * elects to include this software in this distribution under the [CDDL or GPL
- * Version 2] license."  If you don't indicate x single choice of license, x
+ * Version 2] license."  If you don't indicate a single choice of license, a
  * recipient has the option to distribute your version of this file under
  * either the CDDL, the GPL Version 2 or to extend the choice of license to
  * its licensees as provided above.  However, if you add GPL Version 2 code
--- tests/performance/test-cases/proxy-injection/src/main/java/org/glassfish/jersey/tests/performance/proxy/injection/JerseyApp.java
+++ tests/performance/test-cases/proxy-injection/src/main/java/org/glassfish/jersey/tests/performance/proxy/injection/JerseyApp.java
@@ -8,7 +8,7 @@
  * and Distribution License("CDDL") (collectively, the "License").  You
  * may not use this file except in compliance with the License.  You can
  * obtain a copy of the License at
- * https://glassfish.dev.java.net/public/CDDL+GPL_1_1.html
+ * http://glassfish.java.net/public/CDDL+GPL_1_1.html
  * or packager/legal/LICENSE.txt.  See the License for the specific
  * language governing permissions and limitations under the License.
  *
