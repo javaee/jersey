21da4e39a1131c8e0a726cebcad89591b0d1d7b7
Libor Kramolis <libor.kramolis@oracle.com>
2013-12-07 22:52:11 +0100

    JERSEY-2176: "Problems with calling close() and flush() methods on underlying output stream." Groovy/Spock unit tests.
    Change-Id: Iaf25815fad69bedd32e32721a3db76d7e61c649b

---
+++ containers/jersey-servlet-core/src/test/groovy/org/glassfish/jersey/servlet/internal/ResponseWriterSpockTest.groovy
@@ -0,0 +1,109 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 2013 Oracle and/or its affiliates. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License.  You can
+ * obtain a copy of the License at
+ * http://glassfish.java.net/public/CDDL+GPL_1_1.html
+ * or packager/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at packager/legal/LICENSE.txt.
+ *
+ * GPL Classpath Exception:
+ * Oracle designates this particular file as subject to the "Classpath"
+ * exception as provided by Oracle in the GPL Version 2 section of the License
+ * file that accompanied this code.
+ *
+ * Modifications:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ *
+ * Contributor(s):
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+package org.glassfish.jersey.servlet.internal
+
+import org.glassfish.jersey.server.ContainerResponse
+import org.glassfish.jersey.servlet.spi.AsyncContextDelegate
+
+import javax.servlet.http.HttpServletResponse
+
+import spock.lang.Unroll
+
+import javax.ws.rs.core.Response
+
+/**
+ * @author Libor Kramolis (libor.kramolis at oracle.com)
+ */
+class ResponseWriterSpockTest extends spock.lang.Specification {
+
+    /**
+     * Reproducer tests for JERSEY-2176.
+     */
+    @Unroll
+    def "Calling ResponseWriter.failure with configSetStatusOverSendError= #configSetStatusOverSendError"() {
+        setup:
+        RuntimeException throwable = new RuntimeException("Whatever Throwable instance")
+        ResponseWriter responseWriter = new ResponseWriter(false, configSetStatusOverSendError,
+                Mock(HttpServletResponse), null, null)
+        responseWriter.response.isCommitted() >> false
+
+        when:
+        responseWriter.failure(throwable)
+
+        then:
+        sendErrorCall * responseWriter.response.sendError(500, "Request failed.")
+        setStatusCall * responseWriter.response.setStatus(500, "Request failed.")
+        def error = thrown(RuntimeException)
+        error == throwable
+
+        where:
+        configSetStatusOverSendError || sendErrorCall || setStatusCall
+        true  || 0 || 1
+        false || 1 || 0
+    }
+
+    /**
+     * Reproducer tests for JERSEY-2176.
+     */
+    @Unroll
+    def "Calling ResponseWriter.commit with configSetStatusOverSendError= #configSetStatusOverSendError"() {
+        setup:
+        def status = Response.Status.GONE
+        ResponseWriter responseWriter = new ResponseWriter(false, configSetStatusOverSendError,
+                Mock(HttpServletResponse), Mock(AsyncContextDelegate), null)
+        responseWriter.response.isCommitted() >> false
+        responseWriter.@responseContext.set(Mock(ContainerResponse))
+        responseWriter.getResponseContext().getStatusInfo() >> status
+        responseWriter.getResponseContext().getStatus() >> status.statusCode
+
+        when:
+        responseWriter.commit()
+
+        then:
+        sendErrorCall * responseWriter.response.sendError(status.statusCode, status.reasonPhrase)
+        0 * responseWriter.response.setStatus(_, _)
+
+        where:
+        configSetStatusOverSendError || sendErrorCall
+        false || 1
+        true  || 0
+    }
+
+}
