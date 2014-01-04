4669b35b0c60c51941ff1659aafda0ffa4ff566e
Michal Gajdos <michal.gajdos@oracle.com>
2014-01-04 12:54:05 +0100

    Added package-info.java to client authentication module. Fixed packages in MVC package-info.java.
    Change-Id: Ic58d568fd309e8e3be0e2cee2601e20adb65ce19
    Signed-off-by: Michal Gajdos <michal.gajdos@oracle.com>

--- core-client/src/main/java/org/glassfish/jersey/client/authentication/package-info.java
+++ core-client/src/main/java/org/glassfish/jersey/client/authentication/package-info.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2010-2014 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -37,28 +37,7 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
-
 /**
- * Provides support for Model, View and Controller (MVC).
- * <p/>
- * Given the MVC pattern the Controller corresponds to a resource class, the View to a template referenced by a template name,
- * and the Model to a Java object (or a Java bean).
- * <p/>
- * A resource method of a resource class may return an instance of {@link org.glassfish.jersey.server.mvc.Viewable} that
- * encapsulates the template name and the model. In this respect the instance of{@link org.glassfish.jersey.server.mvc.Viewable}
- * is the response entity. Such a viewable response entity may be set in contexts other than a resource method but for the
- * purposes of this section the focus is on resource methods.
- * <p/>
- * The {@link org.glassfish.jersey.server.mvc.Viewable}, returned by a resource method,
- * will be processed such that the template name is resolved to a template reference that identifies a template capable of
- * being processed by an appropriate view processor.
- * <br/>
- * The view processor then processes template given the model to produce a response entity that is returned to the client.
- * <p/>
- * For example, the template name could reference a Java Server Page (JSP) and the model will be accessible to that JSP. The
- * JSP view processor will process the JSP resulting in an HTML document that is returned as the response entity. (See later
- * for more details.)
- * <p/>
- * Two forms of returning {@link org.glassfish.jersey.server.mvc.Viewable} instances are supported: explicit; and implicit.
+ * Provides core client authentication mechanisms.
  */
-package org.glassfish.jersey.server.mvc;
+package org.glassfish.jersey.client.authentication;
