411f767d3686ee494000741ee60f24419264679c
Michal Gajdos <michal.gajdos@oracle.com>
2014-01-15 18:53:26 +0100

    JERSEY-2274: Shadow ASM classes with jersey specific packageas it uses old ASM version conflicting with other software using ASM 4.x and above JERSEY-2278: Update to ASM 5.0 beta.
     - Updated to HK2 2.2.0
     - Updated to ASM 5.0
     - Repackaging ASM in core-server and jaxrs-ri
     - Updated dependencies in OSGi tests (in OSGi we're not able to drop
       dependency on asm because hk2-locator lists asm in imports)
     - Added ASM dependency to test-framework to make tests pass in IDE
    
    Change-Id: I0a75cccabf524d507ec8c9766e7734ed2d614253
    Signed-off-by: Michal Gajdos <michal.gajdos@oracle.com>

--- tests/osgi/functional/src/test/java/org/glassfish/jersey/osgi/test/util/Helper.java
+++ tests/osgi/functional/src/test/java/org/glassfish/jersey/osgi/test/util/Helper.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2010-2013 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010-2014 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -167,7 +167,8 @@ public class Helper {
                 mavenBundle().groupId("org.glassfish.hk2").artifactId("hk2-utils").versionAsInProject(),
                 mavenBundle().groupId("org.glassfish.hk2.external").artifactId("javax.inject").versionAsInProject(),
                 mavenBundle().groupId("org.glassfish.hk2.external").artifactId("asm-all-repackaged").versionAsInProject(),
-                mavenBundle().groupId("org.glassfish.hk2.external").artifactId("cglib").versionAsInProject(),
+                mavenBundle().groupId("org.glassfish.hk2.external").artifactId("aopalliance-repackaged").versionAsInProject(),
+                mavenBundle().groupId("org.javassist").artifactId("javassist").versionAsInProject(),
 
                 // Grizzly
                 systemPackage("sun.misc"),
