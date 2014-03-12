127cbd1d61d7f76baf6c099948a07878386db0e8
Michal Gajdos <michal.gajdos@oracle.com>
2014-03-12 11:03:34 +0100

    JERSEY-2255: Entity Data Filtering should support inherited fields
     - fixed property getters
     - updated integration test to use getters instead of fields
    
    Change-Id: I8597f020c2c0a4509f837b81bcf4926d93fd85f7
    Signed-off-by: Michal Gajdos <michal.gajdos@oracle.com>

--- tests/integration/jersey-2255/src/main/java/org/glassfish/jersey/tests/integration/jersey2255/Issue2255Resource.java
+++ tests/integration/jersey-2255/src/main/java/org/glassfish/jersey/tests/integration/jersey2255/Issue2255Resource.java
@@ -39,10 +39,23 @@
  */
 package org.glassfish.jersey.tests.integration.jersey2255;
 
+import java.lang.annotation.Annotation;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+
+import org.glassfish.jersey.message.filtering.EntityFiltering;
+
+import org.glassfish.hk2.api.AnnotationLiteral;
 
 /**
  * Test resource.
@@ -53,35 +66,88 @@ import javax.ws.rs.Produces;
 @Consumes("application/json")
 @Produces("application/json")
 public class Issue2255Resource {
-	
-	public static class A {
-		public A() {}
-		public A(String fieldA1) {
-			this.fieldA1 = fieldA1;
-		}
-		public String fieldA1;
-	}
-	
-	public static class B extends A {
-		public B(){}
-		public B(String fieldA1, String fieldB1) {
-			super(fieldA1);
-			this.fieldB1 = fieldB1;
-		}
-		public String fieldB1;
-	}
 
+    public static class A {
+
+        public A() {
+        }
+
+        public A(String fieldA1) {
+            this.fieldA1 = fieldA1;
+        }
+
+        private String fieldA1;
+
+        @Detailed
+        public String getFieldA1() {
+            return fieldA1;
+        }
+
+        public void setFieldA1(final String fieldA1) {
+            this.fieldA1 = fieldA1;
+        }
+    }
+
+    public static class B extends A {
+
+        public B() {
+        }
+
+        public B(String fieldA1, String fieldB1) {
+            super(fieldA1);
+            this.fieldB1 = fieldB1;
+        }
+
+        private String fieldB1;
+
+        public String getFieldB1() {
+            return fieldB1;
+        }
+
+        public void setFieldB1(final String fieldB1) {
+            this.fieldB1 = fieldB1;
+        }
+    }
+
+    @Target({ElementType.TYPE, ElementType.METHOD, ElementType.FIELD})
+    @Retention(RetentionPolicy.RUNTIME)
+    @Documented
+    @EntityFiltering
+    public static @interface Detailed {
+
+        /**
+         * Factory class for creating instances of {@code ProjectDetailedView} annotation.
+         */
+        public static class Factory
+                extends AnnotationLiteral<Detailed>
+                implements Detailed {
+
+            private Factory() {
+            }
+
+            public static Detailed get() {
+                return new Factory();
+            }
+        }
+    }
 
     @Path("A")
     @GET
-    public A getA() {
-        return new A("fieldA1Value");
+    public Response getA(@QueryParam("detailed") final boolean isDetailed) {
+        return Response
+                .ok()
+                .entity(new A("fieldA1Value"), isDetailed ? new Annotation[]{ Detailed.Factory.get() } : new Annotation[0])
+                .build();
     }
-    
+
     @Path("B")
     @GET
-    public B getB() {
-        return new B("fieldA1Value", "fieldB1Value");
+    public Response getB(@QueryParam("detailed") final boolean isDetailed) {
+        return Response
+                .ok()
+                .entity(new B("fieldA1Value", "fieldB1Value"), isDetailed ?
+                        new Annotation[]{ Detailed.Factory.get() } : new Annotation[0])
+                .build();
     }
 
 }
