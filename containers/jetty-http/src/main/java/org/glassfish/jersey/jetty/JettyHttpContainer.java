99cafb39a980959639e431d2cf0239a193e59c21
Jakub Podlesak <jakub.podlesak@oracle.com>
2015-01-09 11:33:42 +0100

    Fix for JERSEY-2750: @RolesAllowed does not work in JettyHttpContainer
    Change-Id: I8b897cf27221b9a63b9f5f2e1e52f7aabedc692a
    Signed-off-by: Jakub Podlesak <jakub.podlesak@oracle.com>

--- containers/jetty-http/src/main/java/org/glassfish/jersey/jetty/JettyHttpContainer.java
+++ containers/jetty-http/src/main/java/org/glassfish/jersey/jetty/JettyHttpContainer.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2013-2014 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013-2015 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -211,7 +211,7 @@ public final class JettyHttpContainer extends AbstractHandler implements Contain
 
             @Override
             public boolean isUserInRole(final String role) {
-                return false;
+                return request.isUserInRole(role);
             }
 
             @Override
