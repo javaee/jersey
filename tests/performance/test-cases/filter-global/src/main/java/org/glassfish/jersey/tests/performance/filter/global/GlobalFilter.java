73437cae47dbee0cf60d0813bc8dabb13217893d
Jakub Podlesak <jakub.podlesak@oracle.com>
2015-02-24 03:20:40 +0100

    + Introduced JMX agent to simplify performance testing (single agent enables code instrumentation at runtime to allow HTTP container/JAX-RS implementation agnostic testing) + Rewritten existing performance tests to get rid of unnecessary Jersey/Grizzly dependencies when applicable + Introduced Grizzly/Jersey based runner + Reintroduced parameter-less MBW test case apps + Added functional tests to all perf test cases
    Change-Id: Ic515b078871f7f443928c9679f77511763ca7baa
    Signed-off-by: Jakub Podlesak <jakub.podlesak@oracle.com>

--- tests/performance/test-cases/filter-global/src/main/java/org/glassfish/jersey/tests/performance/filter/global/GlobalFilter.java
+++ tests/performance/test-cases/filter-global/src/main/java/org/glassfish/jersey/tests/performance/filter/global/GlobalFilter.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2013 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013-2015 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -59,12 +59,14 @@ import javax.ws.rs.ext.Provider;
 @Provider
 public class GlobalFilter implements ContainerRequestFilter, ContainerResponseFilter {
 
+    @Override
     public void filter(ContainerRequestContext requestContext) throws IOException {
         if (requestContext.hasEntity()) {
             requestContext.setEntityStream(new SequenceInputStream(new ByteArrayInputStream("PRE_MATCH_IN".getBytes()), requestContext.getEntityStream()));
         }
     }
 
+    @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
         if (responseContext.hasEntity()) {
             responseContext.setEntity("" + responseContext.getEntity() + "PRE_MATCH_OUT", null, MediaType.TEXT_PLAIN_TYPE);
--- tests/performance/test-cases/interceptor-dynamic/src/main/java/org/glassfish/jersey/tests/performance/interceptor/dynamic/DynamicallyBoundInterceptor.java
+++ tests/performance/test-cases/interceptor-dynamic/src/main/java/org/glassfish/jersey/tests/performance/interceptor/dynamic/DynamicallyBoundInterceptor.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2013 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013-2015 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -58,11 +58,13 @@ import javax.ws.rs.ext.WriterInterceptorContext;
 @Provider
 public class DynamicallyBoundInterceptor implements ReaderInterceptor, WriterInterceptor {
 
+    @Override
     public Object aroundReadFrom(ReaderInterceptorContext context) throws IOException, WebApplicationException {
         context.setInputStream(new SequenceInputStream(new ByteArrayInputStream("READ ".getBytes()), context.getInputStream()));
         return context.proceed();
     }
 
+    @Override
     public void aroundWriteTo(WriterInterceptorContext context) throws IOException, WebApplicationException {
 	context.getOutputStream().write("WRITE ".getBytes());
         context.proceed();
