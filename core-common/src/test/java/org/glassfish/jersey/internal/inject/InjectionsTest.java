82bd69416f9656e37ff5bbfebe89abe163b8b1d5
Jord Sonneveld <jord@moz.com>
2015-06-24 09:32:48 -0700

    JERSEY-2291: Address review comments.
    Add copyright header.
    Reformat to use 4 space indents.

--- core-common/src/test/java/org/glassfish/jersey/internal/inject/InjectionsTest.java
+++ core-common/src/test/java/org/glassfish/jersey/internal/inject/InjectionsTest.java
@@ -1,3 +1,43 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 2015 Oracle and/or its affiliates. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License.  You can
+ * obtain a copy of the License at
+ * http://glassfish.java.net/public/CDDL+GPL_1_1.html
+ * or packager/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at packager/legal/LICENSE.txt.
+ *
+ * GPL Classpath Exception:
+ * Oracle designates this particular file as subject to the "Classpath"
+ * exception as provided by Oracle in the GPL Version 2 section of the License
+ * file that accompanied this code.
+ *
+ * Modifications:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ *
+ * Contributor(s):
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+
 package org.glassfish.jersey.internal.inject;
 
 import static org.junit.Assert.assertTrue;
@@ -20,48 +60,49 @@ import org.junit.Test;
  */
 public class InjectionsTest {
 
-  /**
-   * Verify that services marked with the HK2 Immediate annotation are indeed
-   * created "immediately" (or at least "soon").
-   *
-   * Because Immediate services are instantiated in a separate thread, we use a
-   * {@link CountDownLatch} to wait for the service to be created.
-   *
-   * After the {@link ServiceLocator} is created, we specifically do not call
-   * any more methods on it: the locator must instantiate the Immediate service
-   * without any further prompting to the locator.
-   *
-   * @throws InterruptedException
-   *           if awaiting on the latch is interrupted.
-   */
-  @Test
-  public void testHK2ImmediateAnnotation() throws InterruptedException {
-    final CountDownLatch latch = new CountDownLatch(1);
+    /**
+     * Verify that services marked with the HK2 Immediate annotation are indeed
+     * created "immediately" (or at least "soon").
+     *
+     * Because Immediate services are instantiated in a separate thread, we use
+     * a {@link CountDownLatch} to wait for the service to be created.
+     *
+     * After the {@link ServiceLocator} is created, we specifically do not call
+     * any more methods on it: the locator must instantiate the Immediate
+     * service without any further prompting to the locator.
+     *
+     * @throws InterruptedException if awaiting on the latch is interrupted.
+     */
+    @Test
+    public void testHK2ImmediateAnnotation() throws InterruptedException {
+        final CountDownLatch latch = new CountDownLatch(1);
 
-    @SuppressWarnings("unused") // It is unused by design
-    ServiceLocator sl = Injections.createLocator(new AbstractBinder() {
-      @Override
-      protected void configure() {
-        bind(latch).to(CountDownLatch.class);
-        bind(ImmediateMe.class).to(ImmediateMe.class).in(Immediate.class);
-      }
-    });
+        @SuppressWarnings("unused") // It is unused by design
+        ServiceLocator sl = Injections.createLocator(new AbstractBinder() {
+            @Override
+            protected void configure() {
+                bind(latch).to(CountDownLatch.class);
+                bind(ImmediateMe.class).to(ImmediateMe.class).in(
+                        Immediate.class);
+            }
+        });
 
-    // 10 seconds is a LONG time. It should be faster than that. However, 10
-    // seconds gives us a reasonable upper limit to wait in case the test fails.
-    assertTrue("Latch should be unlocked within 10 seconds.",
-        latch.await(10, TimeUnit.SECONDS));
-  }
-
-  /**
-   * Helper class for testing Immediate services.
-   *
-   */
-  public static final class ImmediateMe {
-    @Inject
-    public ImmediateMe(CountDownLatch latch) {
-      latch.countDown();
+        // 10 seconds is a LONG time. It should be faster than that. However, 10
+        // seconds gives us a reasonable upper limit to wait in case the test
+        // fails.
+        assertTrue("Latch should be unlocked within 10 seconds.",
+                latch.await(10, TimeUnit.SECONDS));
     }
-  }
+
+    /**
+     * Helper class for testing Immediate services.
+     *
+     */
+    public static final class ImmediateMe {
+        @Inject
+        public ImmediateMe(CountDownLatch latch) {
+            latch.countDown();
+        }
+    }
 
 }
