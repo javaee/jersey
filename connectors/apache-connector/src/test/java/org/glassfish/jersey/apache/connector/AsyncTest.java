f182eeb947f4b1d1bed386a3a68bc24bdb575581
Petr Bouda <petr.bouda@oracle.com>
2015-07-08 11:09:18 +0200

    fix windows build
    Change-Id: I225043edbd4ae703c89be9e34ec3f333f4b7a46e

--- connectors/apache-connector/src/test/java/org/glassfish/jersey/apache/connector/AsyncTest.java
+++ connectors/apache-connector/src/test/java/org/glassfish/jersey/apache/connector/AsyncTest.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2013-2014 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013-2015 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -242,7 +242,7 @@ public class AsyncTest extends JerseyTest {
 
         final int asyncTimeoutMultiplier = getAsyncTimeoutMultiplier();
         LOGGER.info("Using async timeout multiplier: " + asyncTimeoutMultiplier);
-        assertThat("Async processing took too long.", toc - tic, Matchers.lessThan(3 * AsyncResource.OPERATION_DURATION
+        assertThat("Async processing took too long.", toc - tic, Matchers.lessThan(4 * AsyncResource.OPERATION_DURATION
                 * asyncTimeoutMultiplier));
 
     }
