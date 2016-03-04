c35de196b1bdf09128f43761970ec48d1794beab
Pavel Bucek <pavel.bucek@oracle.com>
2016-03-04 19:01:28 +0100

    JERSEY-3069: LanguageTag#equals(...) fix.
    Change-Id: I040a43117a1979db8beb5d5d8498fa6e93390525

--- core-common/src/main/java/org/glassfish/jersey/message/internal/LanguageTag.java
+++ core-common/src/main/java/org/glassfish/jersey/message/internal/LanguageTag.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2010-2015 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010-2016 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -178,7 +178,7 @@ public class LanguageTag {
         if (this == o) {
             return true;
         }
-        if (!(o instanceof LanguageTag) || o.getClass() == this.getClass()) {
+        if (!(o instanceof LanguageTag) || o.getClass() != this.getClass()) {
             return false;
         }
 
