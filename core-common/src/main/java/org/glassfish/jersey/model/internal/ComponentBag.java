65790269cb2c029f35a67ecc7310dfc5227048ac
Adam Lindenthal <adam.lindenthal@oracle.com>
2016-03-14 18:07:08 +0100

    Prevent double registering components when injecting managed client
    Change-Id: I4f1c3e6642800cff4e47b1e81f0a49b9bbd07c84
    (cherry picked from commit 927b4d0605aeb119d15e9623dab65c8aec2c8e20)

--- core-common/src/main/java/org/glassfish/jersey/model/internal/ComponentBag.java
+++ core-common/src/main/java/org/glassfish/jersey/model/internal/ComponentBag.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2012-2014 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012-2016 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -400,7 +400,7 @@ public class ComponentBag {
             public Boolean call() {
                 if (models.containsKey(componentClass)) {
                     Errors.error(LocalizationMessages.COMPONENT_TYPE_ALREADY_REGISTERED(componentClass),
-                            Severity.WARNING);
+                            Severity.HINT);
                     return false;
                 }
 
